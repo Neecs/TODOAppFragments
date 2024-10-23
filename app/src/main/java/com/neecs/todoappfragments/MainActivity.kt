6e822c649a8ef5a2a5e56a666811df3d3bf76639
Nicolas Gil <nikosgil12@gmail.com>
2024-10-22 23:02:00 -0500

    two fragments implemented

--- app/src/main/java/com/neecs/todoappfragments/MainActivity.kt
+++ app/src/main/java/com/neecs/todoappfragments/MainActivity.kt
@@ -3,23 +3,16 @@ package com.neecs.todoappfragments
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.neecs.todoappfragments.R
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
         val navController = findNavController(R.id.nav_host_fragment)
-        setupActionBarWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
